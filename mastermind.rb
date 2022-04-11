b76000c39b03577939591fb3a835c767b267009d
benedictfernando <fernandobenedict619@gmail.com>
2022-04-11 10:26:24 +0800

    provide structure for feedback per incorrect guess

--- mastermind.rb
+++ mastermind.rb
@@ -31,7 +31,12 @@ class Mastermind
         # give player chance to guess the code within no. of tries
         attempt, max_tries = 0, 12
 
-        puts "Guess the #{code_to_break} within #{max_tries} moves."
+        puts "Guess the #{code_to_break} within #{max_tries} moves.\n" + 
+            "Note: You'll be provided with feedback for every incorrect guess you make.\n" + 
+            "• '⚫' signifies that you've matched a peg with its exact location against the code.\n" + 
+            "• '⚪' signifies that you've matched a peg, but on an incorrect location against the code.\n"
+
+        puts "\nYou may now input your first guess below. Good luck!"
         print "Guess ##{attempt + 1} > "
 
         while guess = gets.chomp
@@ -39,6 +44,8 @@ class Mastermind
             puts    # print new line for breaks
 
             if guess == code_to_break
+                if attempt == 0 then puts 'Woah, first try!'
+                elsif attempt == 11 then puts "Nice, that's clutch!" end
                 puts "Congratulations~ you win!"; break
             else
                 attempt += 1
@@ -51,6 +58,9 @@ class Mastermind
                 # otherwise, furhter prompt him/her for his/her guess 
                 puts "Your guess is wrong~ try again. " + 
                     "(#{max_tries - attempt} move/s left.)"
+
+                # provide feedback along the way
+                provide_feedback(guess, code_to_break)
                 print "Guess ##{attempt + 1} > "
             end 
         end
@@ -68,6 +78,17 @@ class Mastermind
         code
     end
 
+    def provide_feedback(guess, code_to_break)
+        # check for every character of 'guess' (for loop || use length of 'code_to_break')
+            # if character is exact as w/ exact location vs 'code_to_break'
+                # print '⚫ '
+                # next (skip loop to next iteration)
+            # elsif character has at least one character vs 'code_to_break'
+                # print '⚪ '
+                # next (skip loop to next iteration)
+            # end
+        # end
+    end
 
     def make_code
         puts "You are the code maker."
